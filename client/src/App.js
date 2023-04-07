187d84f36b97e913a94c6c889c8aa5832d36cb0b
Jayram Bagal <94613732+jayrambagal@users.noreply.github.com>
2023-04-07 20:56:07 +0530

    authentication completed

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import tachyons from "tachyons";
+
+function App() {
+  return (
+    <div className="App">
+    
+      hello
+    
+    </div>
+  );
+}
+
+export default App;
